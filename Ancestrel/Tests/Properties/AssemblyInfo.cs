fad17fb8dae6f4593dce6987ea08ffc74f42a3c1
Florian-alexandre-dumas <florian.dumas.al@gmail.com>
2021-12-09 16:43:11 +0100

    Commit initial + doc UML

---
+++ Ancestrel/Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bd974a09-fbc2-4043-8e75-bea4f5f27d77")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Ancestrel/model/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace model
+{
+    public class Class1
+    {
+    }
+}
