13a44aa6be853ec762cdcc1b4aca2ee5e7c10058
Mathieu Villedieu de Torcy <mathieu.villedieu_de_torcy@etu.uca.fr>
2022-02-08 16:06:01 +0100

    modif bdd apres reu yon

--- Ancestrel/BDD/table.sql
+++ Ancestrel/BDD/table.sql
@@ -13,11 +13,11 @@ DROP TABLE IF EXISTS dbo.Image;
 
 
 
-CREATE TABLE [dbo].[Arbre]
-(
-	[Id] INT NOT NULL PRIMARY KEY,
-	[Nom] VARCHAR(100) NOT NULL UNIQUE
-)
+-- CREATE TABLE [dbo].[Arbre]
+-- (
+-- 	[Id] INT NOT NULL PRIMARY KEY,
+-- 	[Nom] VARCHAR(100) NOT NULL UNIQUE
+-- )
 
 CREATE TABLE [dbo].[Prenom]
 (
@@ -52,14 +52,14 @@ CREATE TABLE [dbo].[Ville]
 	[Nom] VARCHAR(200) UNIQUE NOT NULL,
 	[Latitude] DECIMAL(10,7),
 	[Longitude] DECIMAL(10,7),
-	[Pays] INT FOREIGN KEY REFERENCES [Pays]([Id])
+	[Pays] INT FOREIGN KEY REFERENCES [Pays]([Id]) ON DELETE CASCADE
 )
 
 -- CREATE TABLE [dbo].[Sexe]
 -- (
 -- 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
 -- 	[Sexe] VARCHAR(5) UNIQUE NOT NULL,
--- 	CONSTRAINT chk_valeur_sexe CHECK (Sexe.TOUPPER 'FEMME' OR Sexe LIKE 'HOMME')
+-- 	CONSTRAINT chk_valeur_sexe CHECK (Sexe 'FEMME' OR Sexe LIKE 'HOMME')
 -- )
 
 CREATE TABLE [dbo].[Description]
@@ -71,6 +71,7 @@ CREATE TABLE [dbo].[Description]
 CREATE TABLE [dbo].[Personne]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[Sexe] INT NOT NULL,  
 	[Nom_usage] VARCHAR(200) NOT NULL,
 	[Nom] VARCHAR(200) NOT NULL,
 	[Date_naissance] DATE,
@@ -81,10 +82,10 @@ CREATE TABLE [dbo].[Personne]
 	[Id_mere] INT,
 	[Id_sexe] INT,
 	CONSTRAINT fk_perso_ville FOREIGN KEY(Id_ville) REFERENCES Ville(Id),
-	CONSTRAINT fk_perso_img FOREIGN KEY(Id_img_principale) REFERENCES Image(Id),
+	CONSTRAINT fk_perso_img FOREIGN KEY(Id_img_principale) REFERENCES Image(Id) ON DELETE CASCADE,
 	CONSTRAINT fk_perso_pere FOREIGN KEY(Id_pere) REFERENCES Personne(Id),
-	CONSTRAINT fk_perso_mere FOREIGN KEY(Id_mere) REFERENCES Personne(Id),
-	CONSTRAINT fk_perso_sexe FOREIGN KEY(Id_sexe) REFERENCES Sexe(Id)
+	CONSTRAINT fk_perso_mere FOREIGN KEY(Id_mere) REFERENCES Personne(Id) 
+	-- CONSTRAINT fk_perso_sexe FOREIGN KEY(Id_sexe) REFERENCES Sexe(Id) ON DELETE CASCADE
 )
 
 CREATE TABLE [dbo].[Description_Personne]
@@ -92,27 +93,27 @@ CREATE TABLE [dbo].[Description_Personne]
 	[Id_description] INT,
 	[Id_personne] INT,
 	CONSTRAINT pk_descper PRIMARY KEY(Id_description, Id_personne),
-	CONSTRAINT fk_descper_prenom FOREIGN KEY(Id_description) REFERENCES Description(Id),
-	CONSTRAINT fk_descper_personne FOREIGN KEY(Id_personne) REFERENCES Personne(Id)
+	CONSTRAINT fk_descper_prenom FOREIGN KEY(Id_description) REFERENCES Description(Id) ON DELETE CASCADE,
+	CONSTRAINT fk_descper_personne FOREIGN KEY(Id_personne) REFERENCES Personne(Id) ON DELETE CASCADE
 )
 
-CREATE TABLE [dbo].[Arbre_Personne]
-(
-	[Id_arbre] INT,
-	[Id_personne] INT,
-	[Numero] INT,
-	CONSTRAINT pk_arb_pers PRIMARY KEY(Id_arbre, Id_personne),
-	CONSTRAINT fk_arb_pers_arbre FOREIGN KEY(Id_arbre) REFERENCES Arbre(Id),
-	CONSTRAINT fk_arb_pers_personne FOREIGN KEY(Id_personne) REFERENCES Personne(Id)
-)
+-- CREATE TABLE [dbo].[Arbre_Personne]
+-- (
+-- 	[Id_arbre] INT,
+-- 	[Id_personne] INT,
+-- 	[Numero] INT,
+-- 	CONSTRAINT pk_arb_pers PRIMARY KEY(Id_arbre, Id_personne),
+-- 	CONSTRAINT fk_arb_pers_arbre FOREIGN KEY(Id_arbre) REFERENCES Arbre(Id) ON DELETE CASCADE,
+-- 	CONSTRAINT fk_arb_pers_personne FOREIGN KEY(Id_personne) REFERENCES Personne(Id) ON DELETE CASCADE
+-- )
 
 CREATE TABLE [dbo].[Image_Personne]
 (
 	[Id_image] INT,
 	[Id_personne] INT,
 	CONSTRAINT pk_imgper PRIMARY KEY(Id_image, Id_personne),
-	CONSTRAINT fk_imgper_image FOREIGN KEY(Id_image) REFERENCES Image(Id),
-	CONSTRAINT fk_imgper_personne FOREIGN KEY(Id_personne) REFERENCES Personne(Id)
+	CONSTRAINT fk_imgper_image FOREIGN KEY(Id_image) REFERENCES Image(Id) ON DELETE CASCADE,
+	CONSTRAINT fk_imgper_personne FOREIGN KEY(Id_personne) REFERENCES Personne(Id) ON DELETE CASCADE
 )
 
 CREATE TABLE [dbo].[Nationalite]
@@ -120,15 +121,16 @@ CREATE TABLE [dbo].[Nationalite]
 	[Id_pays] INT NOT NULL,
 	[Id_personne] INT NOT NULL,
 	CONSTRAINT pk_nationalite PRIMARY KEY(Id_pays, Id_personne),
-	CONSTRAINT fk_nationalite_pays FOREIGN KEY(Id_pays) REFERENCES Pays(Id),
-	CONSTRAINT fk_nationalite_personne FOREIGN KEY(Id_personne) REFERENCES Personne(Id)
+	CONSTRAINT fk_nationalite_pays FOREIGN KEY(Id_pays) REFERENCES Pays(Id) ON DELETE CASCADE,
+	CONSTRAINT fk_nationalite_personne FOREIGN KEY(Id_personne) REFERENCES Personne(Id) ON DELETE CASCADE
 )
 
 CREATE TABLE [dbo].[Prenom_Personne]
 (
 	[Id_prenom] INT NOT NULL,
 	[Id_personne] INT NOT NULL,
-	CONSTRAINT pk_preper PRIMARY KEY(Id_prenom, Id_personne),
-	CONSTRAINT fk_preper_prenom FOREIGN KEY(Id_prenom) REFERENCES Prenom(Id),
-	CONSTRAINT fk_preper_personne FOREIGN KEY(Id_personne) REFERENCES Personne(Id)
+	[Ordre] INT NOT NULL IDENTITY (1,1),
+	CONSTRAINT pk_preper PRIMARY KEY(Id_prenom, Id_personne, Ordre),
+	CONSTRAINT fk_preper_prenom FOREIGN KEY(Id_prenom) REFERENCES Prenom(Id) ON DELETE CASCADE,
+	CONSTRAINT fk_preper_personne FOREIGN KEY(Id_personne) REFERENCES Personne(Id) ON DELETE CASCADE
 )
