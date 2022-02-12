1c9a595bc5d3306fd1397d4dbc05496611560145
Mathieu Villedieu de Torcy <mathieu.villedieu_de_torcy@etu.uca.fr>
2022-02-12 20:00:32 +0100

    ajout VilleBdd.cs

---
+++ Ancestrel/DataBase/VilleBdd.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    internal class VilleBdd
+    {
+        private readonly string _chaineConnexion;
+
+        private readonly string _VilleTable = "dbo.Ville";
+
+        private readonly string _Id = "Id";
+        private readonly string _Code = "Code";
+        private readonly string _Nom = "Nom";
+        private readonly string _Latitude = "Latitude";
+        private readonly string _Longitude = "Longitude";
+        private readonly string _Pays = "Pays";
+
+
+        public VilleBdd(string chaineConnexion)
+        {
+            _chaineConnexion = chaineConnexion;
+        }
+
+
+    }
+}
